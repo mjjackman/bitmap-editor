91e123ec79c704ce03e50ff918b294459baa9941
mjjackman <mjjackman2000@yahoo.co.uk>
2017-11-09 21:46:31 +0000

    Written a spec for changing the colours in a vertical line

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -10,7 +10,7 @@ describe BitmapEditor do
 		context "given the numbers 5, 6" do
 			it "returns a 2D array with 5 columns and 6 rows" do
 
-				@test_bitmap = @editor.new_image(5,6)
+				@test_bitmap = @editor.new_image(5, 6)
 				expect(@test_bitmap).to eql([['O','O','O','O','O'],
 																		['O','O','O','O','O'],
 																		['O','O','O','O','O'],
@@ -22,7 +22,7 @@ describe BitmapEditor do
 
 		context "given the numbers 2, 4" do
 			it "returns a 2D array with 2 columns and 4 rows" do
-				@test_bitmap = @editor.new_image(2,4)
+				@test_bitmap = @editor.new_image(2, 4)
 				expect(@test_bitmap).to eql([['O','O'],
 																		['O','O'],
 																		['O','O'],
@@ -38,13 +38,27 @@ describe BitmapEditor do
 	describe "#colour_px" do
 		context "given the input 1, 3, 'A'" do
 			it "sets the first element in the third array to 'A'" do
-				@editor.colour_px(1,3,'A')
+				@editor.colour_px(1, 3, 'A')
 				expect(@editor.bitmap).to eql([['O','O','O','O','O'],
 																		['O','O','O','O','O'],
 																		['A','O','O','O','O'],
 																		['O','O','O','O','O'],
 																		['O','O','O','O','O'],
 																		['O','O','O','O','O']])
+			end
+		end
+	end
+
+	describe "#colour_vertical_px" do
+		context "given the input 2, 3, 6, 'W'" do
+			it "changes the elements in column 2 rows 3 to 6 inclusively to 'W" do
+				@editor.colour_vertical_px(2, 3, 'W')
+				expect(@editor.bitmap).to eql([['O','O','O','O','O'],
+																		['O','O','O','O','O'],
+																		['O','W','O','O','O'],
+																		['O','W','O','O','O'],
+																		['O','W','O','O','O'],
+																		['O','W','O','O','O']])
 			end
 		end
 	end
