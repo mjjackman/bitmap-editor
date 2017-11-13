259de6ed9237bec35ce2aea49aad3c3d599937be
mjjackman <mjjackman2000@yahoo.co.uk>
2017-11-13 10:32:20 +0000

    Refactored code by creating a Bitmap class and move methods into the class

---
+++ lib/bitmap.rb
@@ -0,0 +1,29 @@
+class Bitmap
+	attr_reader :bitmap
+
+  def initialize(m, n)
+    @m = m
+    @n = n
+    @bitmap = Array.new(n) { Array.new(m, 'O') }
+  end
+
+  def colour_px(x, y, colour)
+    @bitmap[y-1][x-1] = colour
+  end
+
+  def colour_vertical_px(x, y1, y2, colour)
+    @bitmap[(y1-1)..(y2-1)].each {|i| i[x-1] = colour }
+  end
+
+  def colour_horizontal_px(x1, x2, y, colour)
+    @bitmap[y-1][(x1-1)..(x2-1)] = @bitmap[y-1][(x1-1)..(x2-1)].fill(colour)
+  end
+
+  def clear
+    @bitmap.each {|x| x.each {|y| y.replace('O')}}
+  end
+
+  def show
+    @bitmap.map { |i| i.join(' ') }
+  end
+end
--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,3 +1,4 @@
+require './lib/bitmap'
 require 'pry'
 
 class BitmapEditor
@@ -18,32 +19,32 @@ class BitmapEditor
       case command
       when 'I'
         if line.length == 3 && check_params(line, 3)
-          @bitmap = new_image(line[1].to_i, line[2].to_i)
+          @bitmap = Bitmap.new(line[1].to_i, line[2].to_i)
         else
           return invalid_msg(line)
         end
       when 'L'
         if line.length == 4 && check_params(line, 4)
-          colour_px(line[1].to_i, line[2].to_i, line[3])
+          @bitmap.colour_px(line[1].to_i, line[2].to_i, line[3])
         else
           return invalid_msg(line)
         end
       when 'V'
         if line.length == 5 && check_params(line, 5)
-          colour_vertical_px(line[1].to_i, line[2].to_i, line[3].to_i, line[4])
+          @bitmap.colour_vertical_px(line[1].to_i, line[2].to_i, line[3].to_i, line[4])
         else
           return invalid_msg(line)
         end
       when 'H'
         if line.length == 5 && check_params(line, 5)
-          colour_horizontal_px(line[1].to_i, line[2].to_i, line[3].to_i, line[4])
+          @bitmap.colour_horizontal_px(line[1].to_i, line[2].to_i, line[3].to_i, line[4])
         else
           return invalid_msg(line)
         end
       when 'C'
         clear
       when 'S'
-          puts @bitmap.map { |i| i.join(' ') }
+          puts @bitmap.show
       else
           puts 'unrecognised command :('
       end
@@ -64,25 +65,5 @@ class BitmapEditor
 
   def invalid_msg(line)
     print "#{ line } is an invalid command"
-  end
-
-  def new_image(m, n)
-    matrix = Array.new(n) { Array.new(m, 'O') }
-  end
-
-  def colour_px(x, y, colour)
-    @bitmap[y-1][x-1] = colour
-  end
-
-  def colour_vertical_px(x, y1, y2, colour)
-    @bitmap[(y1-1)..(y2-1)].each {|i| i[x-1] = colour }
-  end
-
-  def colour_horizontal_px(x1, x2, y, colour)
-    @bitmap[y-1][(x1-1)..(x2-1)] = @bitmap[y-1][(x1-1)..(x2-1)].fill(colour)
-  end
-
-  def clear
-    @bitmap.each {|x| x.each {|y| y.replace('O')}}
   end
 end
